5adec04dfa3832393beab6a270b1c5f810cfc89c
Richard Lewin <richardlewin@Richards-MacBook-Pro.local>
2018-01-15 14:16:20 +0000

    Initial iOS files added

---
+++ src/ios/FileEncryption.m
@@ -0,0 +1,19 @@
+#import "FileEncryption.h"
+
+@implementation FileEncryption
+
+- (void) pluginInitialize
+{
+    
+}
+
+/**
+ * Enable the mode to stay awake
+ * when switching to background for the next time.
+ */
+- (void) encrypt:(CDVInvokedUrlCommand*)command
+{
+   
+}
+
+@end
