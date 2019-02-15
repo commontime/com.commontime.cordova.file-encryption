ccc0651ec60ca6e5f83cafded976a55db6dd0ef9
Richard Lewin <>
2019-02-15 14:29:34 +0000

    Added function to iOS to get file size and audio file duration to match Android. Increased version number.

--- src/ios/FileEncryption.m
+++ src/ios/FileEncryption.m
@@ -1,4 +1,5 @@
 #import "FileEncryption.h"
+#import <AVFoundation/AVFoundation.h>
 
 NSString* const ENCRYPT_FILE_MESSAGE_ID = @"ENCRYPT_FILE";
 
@@ -44,15 +45,67 @@ NSString* const ENCRYPT_FILE_MESSAGE_ID = @"ENCRYPT_FILE";
     NSString *path = [self crypto:@"decrypt" command:command];
         
     if (path != nil) {
-        pluginResult =
-        [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-                  messageAsString:path];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:path];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
     
-    [self.commandDelegate sendPluginResult:pluginResult
-                                callbackId:command.callbackId];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)getFileSize:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult *pluginResult = nil;
+    
+    NSString *path = [command.arguments objectAtIndex:0];
+    path = [path stringByReplacingOccurrencesOfString:@"file://" withString:@""];
+    path = [path stringByReplacingOccurrencesOfString:@"file:" withString:@""];
+    
+    if (path) {
+        
+        NSError *error = nil;
+        
+        NSDictionary *fileAttributes = [[NSFileManager defaultManager] attributesOfItemAtPath:path error:&error];
+        
+        if (error) {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        } else {
+            NSNumber *fileSizeNumber = [fileAttributes objectForKey:NSFileSize];
+            long long fileSize = [fileSizeNumber longLongValue];
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[NSString stringWithFormat:@"%lld", fileSize]];
+        }
+        
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)getAudioFileDuration:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult *pluginResult = nil;
+    
+    NSString *path = [command.arguments objectAtIndex:0];
+    
+    if (path) {
+        
+        NSError *error = nil;
+        AVAudioPlayer* avAudioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL URLWithString:path] error:&error];
+        
+        if (error) {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        } else {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDouble:avAudioPlayer.duration];
+        }
+        
+        avAudioPlayer = nil;
+        
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (NSString*)crypto:(NSString *)action command:(CDVInvokedUrlCommand *)command {
